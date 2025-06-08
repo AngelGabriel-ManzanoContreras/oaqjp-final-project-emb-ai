a6eb83f4801087355d79c12884ef9ea0ef101620
Angel Gabriel Manzano Contreras <angel.manzano1326@alumnos.udg.mx>
2025-06-08 00:59:02 -0400

    task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,6 +1,5 @@
-# final_project/emotion_detection.py
-
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -8,6 +7,24 @@ def emotion_detector(text_to_analyze):
     input_json = { "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, headers=headers, json=input_json)
-    
-    # Return the JSON text response
-    return response.text
+    result = json.loads(response.text)
+
+    emotion_scores = result['emotionPredictions'][0]['emotion']
+
+    anger = emotion_scores.get('anger', 0)
+    disgust = emotion_scores.get('disgust', 0)
+    fear = emotion_scores.get('fear', 0)
+    joy = emotion_scores.get('joy', 0)
+    sadness = emotion_scores.get('sadness', 0)
+
+    emotions = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+    dominant_emotion = max(emotions, key=emotions.get)
+    emotions['dominant_emotion'] = dominant_emotion
+
+    return emotions
